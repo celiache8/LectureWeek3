5add929f88dbba46e169f23e267c69d914fa8cef
Celia Chen <celiachen0822@gmail.com>
2018-02-07 12:54:43 -0800

    Lecture Example 02/07

--- app/src/main/java/com/example/celiachen/lectureweek3/MainActivity.java
+++ app/src/main/java/com/example/celiachen/lectureweek3/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.celiachen.lectureweek3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to display
+        ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
+
+        // create the adapter
+        RecipeAdapter adapter = new RecipeAdapter(this, recipeList);
+
+        // find the listview in the layout
+        // set the adapter to listview
+        mListView = findViewById(R.id.recipe_list_view);
+        mListView.setAdapter(adapter);
+
+
     }
 }
---
+++ app/src/main/java/com/example/celiachen/lectureweek3/RecipeDetailActivity.java
@@ -0,0 +1,22 @@
+package com.example.celiachen.lectureweek3;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by celiachen on 2/7/18.
+ */
+
+public class RecipeDetailActivity extends AppCompatActivity{
+
+    // override onCreate method
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+
+        // title
+        // instruction url
+
+    }
+}
