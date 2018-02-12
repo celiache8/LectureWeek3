fab86e3bac8937d9432dea5ac1fab91d5949be68
Celia Chen <celiachen0822@gmail.com>
2018-02-12 10:28:59 -0800

    Finished App with intent 02/12

--- app/src/main/java/com/example/celiachen/lectureweek3/MainActivity.java
+++ app/src/main/java/com/example/celiachen/lectureweek3/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.celiachen.lectureweek3;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -9,14 +13,16 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private ListView mListView;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mContext = this;
         // data to display
-        ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
+        final ArrayList<Recipe> recipeList = Recipe.getRecipesFromFile("recipes.json", this);
 
         // create the adapter
         RecipeAdapter adapter = new RecipeAdapter(this, recipeList);
@@ -27,5 +33,31 @@ public class MainActivity extends AppCompatActivity {
         mListView.setAdapter(adapter);
 
 
+        // 1. each row should be clickable
+        // when the row is clicked,
+        // the intent is created and send
+
+        mListView.setOnItemClickListener( new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id){
+                Recipe selectedRecipe = recipeList.get(position);
+
+                // create my intent package
+                // add all the information needed for detail page
+                // startActivity with that intent
+
+                //explicit
+                // from, to
+                Intent detailIntent = new Intent(mContext, RecipeDetailActivity.class);
+                // put title and instruction URL
+                detailIntent.putExtra("title", selectedRecipe.title);
+                detailIntent.putExtra("url", selectedRecipe.instructionUrl);
+
+                startActivity(detailIntent);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/celiachen/lectureweek3/RecipeDetailActivity.java
+++ app/src/main/java/com/example/celiachen/lectureweek3/RecipeDetailActivity.java
@@ -2,12 +2,15 @@ package com.example.celiachen.lectureweek3;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
 
 /**
  * Created by celiachen on 2/7/18.
  */
 
 public class RecipeDetailActivity extends AppCompatActivity{
+
+    private WebView mWebView;
 
     // override onCreate method
     @Override
@@ -17,6 +20,17 @@ public class RecipeDetailActivity extends AppCompatActivity{
 
         // title
         // instruction url
+        // get recipe data from main activity
+
+        String title = this.getIntent().getExtras().getString("title");
+        String url = this.getIntent().getExtras().getString("url");
+
+        // set the title on the action bar
+        setTitle(title);
+
+        // create the webview and load the url
+        mWebView = findViewById(R.id.detail_web_view);
+        mWebView.loadUrl(url);
 
     }
 }
